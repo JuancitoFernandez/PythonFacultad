89e94645e94a1e6f66b02be0f636d96b60a74dfd
juan manuel fernandez <juanmanuelbonin@gmail.com>
2024-06-11 20:26:35 -0300

    modificando parcial 3

--- Parcial/parcial_3.py
+++ Parcial/parcial_3.py
@@ -1,14 +1,4 @@
-def imprimir_inverso(lista):
-    if lista: 
-        print(lista[-1])  
-        imprimir_inverso(lista[:-1])
-    return lista
 
-#uso de lista inversa
-mi_lista = [1, 2, 3, 4, 5]
-imprimir_inverso(mi_lista)
-
-print()
 # Lista de Jedi con su respectiva información
 jedis = [
     {
